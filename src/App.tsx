6148453c6fed98e63f302d0db2724f633f4d6665
伊藤輝良 <kiraito@itouakiraryounoMacBook-Pro.local>
2022-11-19 12:18:22 +0900

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [text, setText] = useState();
+
+  const header = {
+    'X-API-KEY': "xppaYiZWhs0Y2kME8txobpVshHqg9t4U03Sy1Kr6"
+  }
+
+  axios
+    .get("https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=13"
+      , { headers: header })
+    .then((res) => console.log(res.data.result.data[0].data))
+
+  async function f() {
+
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve("done!"), 3000)
+    });
+    console.log(promise); // [[PromiseState]] :"pending",[[PromiseResult]]: undefined
+
+    // 当然asyncの戻り値のPromiseオブジェクトがrejectした場合は、下記以降は実行されない  
+    let result = await promise; // promise が解決するまで待ちます (*)
+    console.log(promise); // [[PromiseState]] :"fulfilled",[[PromiseResult]]: "done!"
+    alert(result); // 3秒後に"done!"アラートが表示
+  }
+  f();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
